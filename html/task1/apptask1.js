c70d760508c336a6764f7b1938c4bb166a8576d6
Maximmaksimverikovskij@gmail.com <maksimverikovskij@gmail.com>
2021-03-18 20:06:58 +0300

    Упрощено создание матрицы. Клетки матрицы заливаются голубым светом. Не доделана функция обработки Начала, стена, Конец(это всё должно быть кликабельным чтобы изменялся цвет).

--- html/task1/apptask1.js
+++ html/task1/apptask1.js
@@ -1,68 +1,41 @@
-
-
+//Переменная матрицы
 const matrix = document.getElementById('matrix')
+
+//Ожидание нажатия кнопки 
 let btn = document.querySelector('button');
 btn.addEventListener('click', CreateTab);
 
-function CreateTab() {
+
+//Создание таблицы по клеткам
+function CreateTab()
+ {
     matrix.innerHTML='';
+    matrix.hidden = false;
     let nnum = document.getElementById('nnum').value;
-    for (let y = 0; y < nnum; y++) {
+
+    for (let i = 0; i < nnum; i++) 
+    {
         let rows = document.createElement('tr');
-        rows.setAttribute('id', `row[${y}]`);
-        matrix.appendChild(rows);
-        for (let x = 0; x < nnum; x++) {
-            let col = document.createElement('td');
-            col.innerText = '';
-            col.setAttribute('id', `col[${y}][${x}]`)
-            rows.appendChild(col);
+
+        for (let j = 0; j < nnum; j++) 
+        {
+            let element = document.createElement('td');
+            
+            element.className = "elem";
+            element.name = "em";
+            element.onclick;
+            element.id = i*nnum+j;
+            rows.append(element);
         }
+        matrix.append(rows);
     }
 }
 
-function GetRandom() {
-    var y = Math.random();
-    if (y < 0.3)
-        y = 0;
-    else
-        y = 1;
-    return y;
+// То что будет происходить при нажатии на элемент таблицы 
+//(Если была нажата до этого кнопка start, то необходимо покрасить клетку в зелёный. 
+//2 Вариант до этого была нажата кнопка стена, то покрасить в чёрный(по выбору).
+//3 вариант: была прожата кнопка конец, то покрасить в жёлтый(по выбору)).
+function obrabotka()
+{
+    
 }
-
-function CreateMatrix(n) {
-    const matrix = [];
-    for (let y = 0; y < n; y++) {
-        const row = [];
-        for (let x = 0; x < n; x++) {
-            if (GetRandom() == 1) {
-                row.push(true);
-            }
-            else {
-                row.push(false);
-            }
-        }
-        matrix.push(row);
-    }
-    return matrix;
-}
-
-function DrawMaze(n, matrix) {
-    canvas.width = PADDING * 2 + n * cellSize;
-    canvas.height = PADDING * 2 + n * cellSize;
-    context.beginPath();
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = backgroundColor;
-    context.fill();
-
-    for (let y = 0; y < n; y++) {
-        for (let x = 0; x < n; x++) {
-            const color = matrix[y][x] ? freeColor : wallColor;
-            context.beginPath();
-            context.rect(PADDING + x * cellSize, PADDING + y * cellSize, cellSize, cellSize);
-            context.fillStyle = color;
-            context.fill();
-        }
-    }
-}
-
-
